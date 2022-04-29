2f3bcc52e7e42e2d238c99cbb660080768224e8b
ilacilac <qkralswl307@gmail.com>
2022-04-29 18:27:48 +0900

    Feat: intro / coment

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -2,9 +2,8 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 
-import First from '../screens/First';
-import Second from '../screens/Second';
-import Third from '../screens/Third';
+import Search from '../screens/Search';
+import Setting from '../screens/Setting';
 
 const Tab = createMaterialTopTabNavigator();
 
@@ -13,25 +12,22 @@ function Tabs() {
     <Tab.Navigator
       screenOptions={{
         tabBarScrollEnabled: true,
-        tabBarItemStyle: {width: 80},
+        tabBarItemStyle: {width: 77},
       }}>
-      <Tab.Screen name="추천" component={First} />
-      <Tab.Screen name="코로나19" component={Second} />
-      <Tab.Screen name="정치" component={Third} />
-      <Tab.Screen name="경제" component={Third} />
-      <Tab.Screen name="사회" component={Third} />
-      <Tab.Screen name="국제" component={Third} />
-      <Tab.Screen name="연애" component={Third} />
-      <Tab.Screen name="스포츠" component={Third} />
-      <Tab.Screen name="IT" component={Third} />
-      <Tab.Screen name="문화" component={Third} />
+      <Tab.Screen name="추천" component={Search} />
+      <Tab.Screen name="코로나19" component={Setting} />
+      <Tab.Screen name="정치" component={Search} />
+      <Tab.Screen name="경제" component={Setting} />
+      <Tab.Screen name="사회" component={Search} />
+      <Tab.Screen name="국제" component={Setting} />
+      <Tab.Screen name="연애" component={Search} />
+      <Tab.Screen name="스포츠" component={Setting} />
+      <Tab.Screen name="IT" component={Search} />
+      <Tab.Screen name="문화" component={Setting} />
     </Tab.Navigator>
   );
 }
 
 export default function AppNavigator() {
-  return (
-    <Tabs />
-    // </NavigationContainer>
-  );
+  return <Tabs />;
 }
