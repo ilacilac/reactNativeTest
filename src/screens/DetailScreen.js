c7632983a453db3f41700619593c34d02b91921e
ilacilac <dev.ilacilac@gmail.com>
2022-05-06 14:53:07 +0900

    Feat: Detail page / webview -> RN data

---
+++ src/screens/DetailScreen.js
@@ -0,0 +1,20 @@
+import {Text, View, StyleSheet, StatusBar} from 'react-native';
+import React from 'react';
+// import {getStatusBarHeight} from 'react-native-status-bar-height';
+
+function DetailScreen() {
+  return (
+    <View>
+      <Text>DetailScreen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  // todo : theme color
+  // detailWrap: {
+  //   marginTop: getStatusBarHeight(),
+  // },
+});
+
+export default DetailScreen;
--- src/screens/RootStack.js
+++ src/screens/RootStack.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import {Button} from 'react-native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-
-import WriteScreen from './WriteScreen';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import DetailScreen from './DetailScreen';
 import BottomNavigator from '../navigation/BottomNavigator';
 
 const Stack = createNativeStackNavigator();
@@ -15,9 +16,19 @@ function RootStack() {
         options={{headerShown: false}}
       />
       <Stack.Screen
-        name="Write"
-        component={WriteScreen}
-        options={{headerShown: false}}
+        name="Detail"
+        component={DetailScreen}
+        options={{
+          headerRight: () => (
+            // <Button
+            //   onPress={() => alert('This is a button!')}
+            //   title="저장"
+            //   color="#333"
+            // />
+            // todo : bookmark 아닐경우 : bookmark-border
+            <Icon name="bookmark" size={24} color={'#333333'} />
+          ),
+        }}
       />
     </Stack.Navigator>
   );
