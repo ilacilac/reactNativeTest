161835043a3882a06315ef76a84fbbf9d2e21088
ilacilac <dev.ilacilac@gmail.com>
2022-05-17 14:46:16 +0900

    Merge

--- src/screens/DetailScreen.js
+++ src/screens/DetailScreen.js
@@ -10,19 +10,10 @@ function DetailScreen({route, navigation}) {
     data,
   };
 
-<<<<<<< HEAD
-=======
-  const handleOnMessage = ({nativeEvent: {data}}) => {
-    // data에 웹뷰에서 보낸 값이 들어옵니다.
-    console.log(data);
-  };
-
->>>>>>> 201eea01d9e141eb1ca7e7db62f14ada499523ba
   return (
     <WebView
       source={{uri: `http://localhost:3000/post`}}
       onLoadEnd={() => webviewRef.postMessage(JSON.stringify(sendData))}
-      onMessage={handleOnMessage}
       ref={ref => setWebviewRef(ref)}
     />
   );
